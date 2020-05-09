e65ca803043862865ed8a3a5af942b2142364bb2
Leon <silenceace@gmail.com>
2020-05-09 20:25:10 +0800

    fixed

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -13,10 +13,10 @@ echo -e "$CRON_STRINGS\n" > /var/spool/cron/crontabs/CRON_STRINGS
 
 chmod -R 0644 /var/spool/cron/crontabs
 
-# crond running in background and log file reading every second by tail to STDOUT
-crond -s /var/spool/cron/crontabs -b -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
-
 # run once start
 if [ -n "$DUMP_ONCE_START" -a "$DUMP_ONCE_START" = "true" ]; then
-    /cron-backup.sh >> /var/log/cron/cron.log 2>&1
+    /cron-backup.sh >> /var/log/cron/cron.log 2>&1 &
 fi
+
+# crond running in background and log file reading every second by tail to STDOUT
+crond -s /var/spool/cron/crontabs -b -L /var/log/cron/cron.log "$@" && tail -f /var/log/cron/cron.log
